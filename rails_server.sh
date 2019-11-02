dfef2c01b53dc834b8da4c091c3f690813b1b4b2
crossedheadx <47150270+crossedheadx@users.noreply.github.com>
2019-11-02 22:54:08 +0100

    fix: change wrong commands
    Change wrong commands and fix a comment into a more update one

--- rails_server.sh
+++ rails_server.sh
@@ -4,16 +4,16 @@
 
 ##System updates functions
 function update {
-  sudo apt update 
+  sudo apt update
 }
 
 function full-up {
-  sudo apt update 
+  sudo apt update
   sudo apt full-upgrade -y
 }
 
 function tipOne {
-  echo "Remeber: after the installation of PostgreSQL do a change in /etc/postgres/x/main/pg_hba.conf"
+  echo "Remeber: after the installation of PostgreSQL do a change in /etc/postgresql/[version]/main/pg_hba.conf"
   echo "change line "
   echo "#TYPE DATABASE  USER  ADDRESS METHOD"
   echo "local all       all           peer"
@@ -26,39 +26,38 @@ function tipOne {
 function ssh-optimization {
   sudo sed -i "s/# Port 22/Port 2304" /etc/ssh/sshd_config
   sudo sed -i "s/PermitRootLogin yes/PermitRootLogin no" /etc/ssh/sshd_config
-  sudo systemctl restart sshd 
+  sudo systemctl restart sshd
 }
 
 function install-all {
-  sudo install -y build-essential dirmngr gnupg ruby ruby-dev zlib1g-dev libruby libssl-dev libpcre3-dev libcurl4-openssl-dev rake ruby-rack nodejs
+  sudo apt install -y build-essential dirmngr gnupg ruby ruby-dev zlib1g-dev libssl libruby-dev libpcre3-dev libcurl4-openssl-dev rake ruby-rack nodejs
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-  update
-  sudo install -y dirmngr curl 
+  sudo apt update
+  sudo apt install -y dirmngr curl
   curl -sSL https://get.rvm.io | bash -s stable --ruby
   source $HOME/.rvm/scripts/rvm
   gem install rails #Ruby on Rails installation
-  sudo apt install -y nginx 
+  sudo apt install -y nginx
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
   sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger bionic main > /etc/apt/sources.list.d/passenger.list'
-  sudo apt install -y apt-transport-https ca-certificates 
+  sudo apt install -y apt-transport-https ca-certificates
   update
-  sudo install -y libnginx-mod-http-passenger 
+  sudo apt install -y libnginx-mod-http-passenger
   sudo systemctl restart nginx
   #databases
-  sudo apt install -y postgresql postgresql-contrib postgresql-server-dev-* libpq-dev  
-  tipOne 
+  sudo apt install -y postgresql postgresql-contrib postgresql-server-dev-* libpq-dev
+  tipOne
   sleep 2
-  sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8 
-  sudo apt install -y sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mirror.mva-n.net/mariadb/repo/10.3/ubuntu bionic main' 
+  sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+  sudo apt install -y sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mirror.mva-n.net/mariadb/repo/10.3/ubuntu bionic main'
   update
   sudo apt install -y mariadb-server libmysqlclient-dev
 }
 
 #BEGIN
 echo 'start initial upgrade check'
-full-up 
+full-up
 echo 'start full install'
-install-all 
+install-all
 echo 'securing ssh'
 ssh-optimization
-
